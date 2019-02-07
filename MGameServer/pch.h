0c231e325fa3cbe71d487ba19bcd1da1e66427fb
anyc123 <anyc123@hanmail.net>
2019-02-07 19:09:14 +0900

    Start

--- MGameServer/pch.h
+++ MGameServer/pch.h
@@ -10,5 +10,14 @@
 #define PCH_H
 
 // TODO: 여기에 미리 컴파일하려는 헤더 추가
+#include <iostream>
+#include <map>
+#include <list>
+#include <queue>
+#include <vector>
+#include <functional>
+#include <string>
+
+using namespace std;
 
 #endif //PCH_H
